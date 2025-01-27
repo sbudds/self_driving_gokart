eb50fbf1eb0b74261f011cd100e3080bf4d52311
Soumit Buddhala <102396753+sbudds@users.noreply.github.com>
2025-01-26 18:35:11 -0800

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,35 @@
+// Define motor control pins
+const int EN = 9;  // Enable pin for motor
+const int IN1 = 7; // Input 1 for motor
+const int IN2 = 8; // Input 2 for motor
+
+void setup() {
+  // Set motor control pins as outputs
+  pinMode(EN, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  // Start serial communication
+  Serial.begin(9600);
+
+  // Start motor by default (forward direction)
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(EN, 255); // Full speed
+}
+
+void loop() {
+  // Check if data is available on the serial port
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n'); // Read the incoming command
+
+    if (command == "STOP") {
+      // Stop the motor
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      analogWrite(EN, 0); // Disable motor
+
+
+    }
+  }
+}
